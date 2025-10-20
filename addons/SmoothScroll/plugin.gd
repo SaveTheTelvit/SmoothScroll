0f3a336b45dfad661442e93b6f4422ed231b952f
SaveTheTelvit <132656005+SaveTheTelvit@users.noreply.github.com>
2025-10-20 14:04:39 +0300

    changed plugin.gd load logic

--- addons/SmoothScroll/plugin.gd
+++ addons/SmoothScroll/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("SmoothScroll", "Container", preload("smooth_scroll.gd"), preload("SmoothScroll.svg"))
-	add_custom_type("BarSmoothScroll", "SmoothScroll", load("bar_smooth_scroll.gd"), preload("SmoothScroll.svg"))
+	add_custom_type("BarSmoothScroll", "SmoothScroll", preload("bar_smooth_scroll.gd"), preload("SmoothScroll.svg"))
 
 func _exit_tree():
 	remove_custom_type("SmoothScroll")
